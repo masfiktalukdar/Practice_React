d1acf75871bae2671f922868e8293b8525fffce6
Masfik Talukdar <freelancermasfik@gmail.com>
2025-02-22 11:24:16 +0600

    Everyting working fine now

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import "./store.jsx"
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-      <ReactQueryDevtools initialIsOpen={false}/>
       <App />
     </React.StrictMode>
 )
