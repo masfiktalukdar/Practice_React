5cd569c0220ed48565f2c7f65f9067b2f3d61e90
Masfik Talukdar <freelancermasfik@gmail.com>
2025-02-22 11:10:55 +0600

    Cleaning Everything and Delete all old codes

--- src/main.jsx
+++ src/main.jsx
@@ -2,23 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import "./store.jsx"
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 5,
-    }
-  }
-})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <QueryClientProvider client={queryClient}>
     <React.StrictMode>
       <ReactQueryDevtools initialIsOpen={false}/>
       <App />
     </React.StrictMode>
-  </QueryClientProvider>
 )
